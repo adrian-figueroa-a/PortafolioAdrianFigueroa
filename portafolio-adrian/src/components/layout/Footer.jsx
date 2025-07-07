38e321e8518875b1e906842693dfd8ba17282b69
AdrianFigueroaAlmonacid <adri.figueroa@duocuc.cl>
2025-07-07 16:01:33 -0400

    cambios en componentes

--- portafolio-adrian/src/components/layout/Footer.jsx
+++ portafolio-adrian/src/components/layout/Footer.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import "../layout/Footer.css";
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 export const Footer = () => {
   return (
     <div>
       <section className="footer">
+        <h2>Contactame</h2>
+
+        <div className="icon-row">
+          <a
+            href="https://github.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+
+          <a href="mailto:adrian@email.com">
+            <FaEnvelope />
+          </a>
+          <a
+            href="https://linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin />
+          </a>
+        </div>
         <p>Adrian Figueroa</p>
-        <p>2025</p>
       </section>
     </div>
   );
