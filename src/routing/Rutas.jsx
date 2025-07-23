d7ecb3aa6dae0d67a5ce8c3b79e174a459302fcd
adrian <adrian.figueroa.a@gmail.com>
2025-07-23 11:51:31 -0400

    Fix: Correct case in Navbar import

--- src/routing/Rutas.jsx
+++ src/routing/Rutas.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { Navbar } from "../components/layout/navbar";
+import { Navbar } from "../components/layout/Navbar";
 import { Footer } from "../components/layout/footer";
 import Inicio from "../components/pages/Inicio";
 
